655a01bb4c8c88bdea696cc2e20ea229108aa172
MuhammedKK <khaloood2281999@gmail.com>
2019-11-07 02:20:19 +0200

    Uploded Project Files

---
+++ email_sender.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST['submit']))
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $Msg = $_POST['message'];
+
+    $to = 'Malmalki@almalki-trade.sa';
+    $subject = 'Form Submission';
+    $message = 'Name' . $name '\n' . 'Phone' . $phone . '\n' . 'Write The Following :' . '\n\n' $Msg;
+    $header = 'From' . $email;
+
+    if(mail($to, $subject, $message, $header)) {
+        echo '<h1> Sent Successfully! Thank You' . $name . ' , We Will Contact You As Soon As Possible </h1>';
+    }
+    else {
+        echo 'Some Thing Wrong Happened Please Try Again';
+    }
+?>
---
+++ js/plugin.js
@@ -0,0 +1,100 @@
+$(function () {
+
+    $(window).resize(function() {
+        $('.header-content').css('padding-top', ($('header').height() - $('.header-content').height()) / 2 - $('.navbar').height() + 20);
+    });
+
+    $('.header-content').css('padding-top', ($('header').height() - $('.header-content').height()) / 2 - $('.navbar').height() + 20);
+
+    // Next Section
+    $('header .fa-chevron-down').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.about').offset().top
+        }, 800)
+    });
+
+
+    // Full Image Plugin
+    $('fullBackground').fullClip({
+        images : ['imgs/1.jpg', 'imgs/2.jpg', 'imgs/3.jpg']
+    });
+
+    // Change Collapse On Focus 
+
+    $('.navbar-toggle').on('click', function () {
+        let n = 0;
+        if( n === 0 ) {
+            $('.navbar-collapse').css('background', '#333');
+            n++;
+        } else {
+            $('.navbar-collapse').css('background', 'transparent');
+            n = 0;
+        }
+        
+    });
+
+
+    // Testimonials Fade In Slides On By One
+
+    (function autoSlider() {
+        
+        $('.slider .active').each(function () {
+            
+           
+            if (!$(this).is(':last-child')) {
+                
+                
+                $(this).delay(800).fadeOut(1000, function () {
+                    
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+                    
+                    autoSlider();
+                    
+                });
+                
+            } else {
+                
+                $(this).delay(800).fadeOut(1000, function () {
+                    
+                    
+                    $(this).removeClass('active');
+                    
+                    $('.slider div').eq(0).addClass('active').fadeIn();
+                    
+                    autoSlider();
+                });
+            }
+            
+        });
+        
+    }());
+
+
+    // Go To Contact Us 
+
+    $('.header-content a').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate( {
+            scrollTop: $('#contact').offset().top
+        }, 1000);
+    });
+
+    // Set Background To Navbar On Scroll Down
+
+    $(window).scroll(function () {
+        if($(this).scrollTop() > $('header').height()) {
+            $('.navbar').css('background', '#2c3e50');
+        } else {
+            $('.navbar').css('background', 'transparent');
+        }
+    });
+
+    // Fade Out Lodaing Page When Page Downloaded
+
+    window.onload = function() {
+        $('.spinner-box .spinner').fadeOut(500, function() {
+            $(this).parent().fadeOut(600);
+        });
+    }
+    
+});
